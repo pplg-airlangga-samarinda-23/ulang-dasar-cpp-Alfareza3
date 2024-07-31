81d48e5a2e14c58ac4bd727e57f8362f75207d75
Dimas_Fahri_Alfareza <dimasfaal@gmail.com>
2024-07-31 11:28:16 +0800

    Tugas Selesai

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 using namespace std;
 
+void cetakData(string nama, string jurusan, string nisn , int tinggibadan)
+{
+    
+    cout<<"Nama: "<< nama << endl;
+    cout<<"Jurusan: "<< jurusan << endl;
+    cout<<"NISN: "<< nisn << endl;
+    cout<<"Tinggi Badan: " << tinggibadan << endl;
+}
+
 int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
+    string nama;
+    string jurusan;
+    string nisn;
+    int tinggibadan;
+
+    cout<<"Masukkan Nama: ";
+    cin>> nama;
+
+    cout<<"Masukkan Jurusan: ";
+    cin>> jurusan;
+
+    cout<<"Masukkan NISN: ";
+    cin>> nisn;
+
+    cout<<"Masukkan Tinggi Badan (cm): ";
+    cin>> tinggibadan;
+
+
+    cetakData(nama, jurusan, nisn, tinggibadan);
+
     return 0;
 }
